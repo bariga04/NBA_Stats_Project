8ac57adfcb16e4c017d27a6b330e797a2e3db339
ramariga <ramariga11@gmail.com>
2023-10-29 16:18:47 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,67 @@
+import java.util.Scanner;
+import java.util.ArrayList; // import just the ArrayList class
+
 public class Main {
 
     public static void main(String[] args) {
 
 
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Player> playerList = new ArrayList<Player>();
+
+
+        //ArrayList<Player> playerList = new ArrayList<Player>();
+
         /**
          * Code to Scan Player Stats In
          */
 
+        System.out.println("Program: NBA Stat Analysis");
+        System.out.println("Author: Bhargavram Ariga\n");
+
+        System.out.println("Welcome User! The following programs computes different stats I have created" +
+                "to quantify a player's: defensive effort, how much of an \"underdog\" they are (essentially the" +
+                "the relative impact they've had based on the number of minutes they were given), " +
+                "their tendency to shoot three pointers, and finally their \"longevity\" which is essentially" +
+                "their relative performance based on how long they've been playing. These four stats labeled:" +
+                "\"defensive effort\", \"underdog\", \"three point affinity\" and \"longevity\" respectively" +
+                "will be computed for different players you input into the program. To input a player simply" +
+                "type their first and last name and copy paste their career stats from statmuse.com. Do all " +
+                "of this on the same line in the terminal. On the " + "statmuse website, simpy highlight the row " +
+                "with their career stats and copy paste it into " + "the terminal. If choose to give more than " +
+                "one player, the program will tell you the best " + "player in specefic categories including the " +
+                "afformentioned stats");
+
+
+        String start;
+
+        while(!start.equals("stop")){
+
+            System.out.println("\nType in the player and their stats. If you want to exit the program type " +
+                    "\"stop\"");
+            System.out.println("\n");
+
+            start = scan.next();
+
+            if(start.toLowerCase().equals("stop")){
+
+                break;
+            }
+
+            ArrayPlayer
+
+        }
+
         /**
          * methods for advanced analytics
          */
+      ArrayList<>
 
-
-        /**
-         * stat print out of top player in each category
-         */
 
         /**
          * code where user can type in player name and program returns ranking for each category
          */
+
 
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,7 @@
+
 public class Player {
 
+    /**
     String name;
     int age;
     int points;
@@ -10,17 +12,35 @@ public class Player {
     double minutes;
     int fta;
     int ftm;
+
+    int steals;
+
+    int blocks;
+
+    int fouls;
+
+    int to;
+
+    double mins;
     int assists;
-    int rebounds;
-    boolean light_injury;
-    boolean medium_injury;
-    boolean high_injury;
+    int offensive_rebounds;
+
+    int def_rebounds;
     double height;
 
-    public Player(int a, int p, int fa, int fm, int ta, int tp,
-    int ftaa, int ftmm, int as, int reb,
-    boolean li, boolean mi, boolean hi, int hei){
+    double PER;
 
+    double fg_percent;
+
+    double tp_percent;
+
+    double underdog;
+
+    public Player(String s, int a, int p, int fa, int fm, int ta, int tp,
+    int ftaa, int ftmm, int steals, int blocks, int fouls, int to, double mins,
+                  int as, int oreb, int dreb, int hei){
+
+        name = s;
         age = a;
         points = p;
         fga = fa;
@@ -29,11 +49,107 @@ public class Player {
         tpm = tp;
         fta = ftaa;
         ftm = ftmm;
+        this.steals = steals;
+        this.blocks = blocks;
+        this.fouls = fouls;
+        this.to = to;
+        mins = this.mins;
         assists = as;
-        rebounds = reb;
-        light_injury = li;
-        medium_injury = mi;
-        high_injury = hi;
+        offensive_rebounds = oreb;
+        def_rebounds = dreb;
         height = hei;
+
+        PER = (fgm*85.910 + steals*53.897 + tpm*51.757 + ftm*46.845
+                + blocks*39.19 + offensive_rebounds*39.190 + assists * 34.677
+                + def_rebounds*14.707 - fouls*17.174 - (fta - ftm)*20.091 - (fga - fgm)*39.190
+                - to*53.897) * (1/mins);
+
+        fg_percent = fgm/fga;
+
+        tp_percent = tpm/tpa;
+
+
+        if(age >= 34 || age <= 22){
+
+            underdog = (tpm*2 + fgm + 0.7*assists -
+                    0.1*mins + 3*steals + 2*blocks - fouls) + 5;
+        }
+        else{
+
+            underdog = tpm*2 + fgm + 0.7*assists - 0.1*mins + 3*steals + 2*blocks - fouls;
+        }
     }
+     */
+
+    String first_name;
+    String last_name;
+    int gp;
+    double mpg;
+    double ppg;
+    double rpg;
+    double apg;
+    double spg;
+    double bpg;
+    double tpg;
+    double fgm;
+    double fga;
+    double fgp;
+    double tpm;
+    double tpa;
+    double ftp;
+    int min;
+    int pts;
+    int reb;
+    int ast;
+    int stl;
+    int blk;
+    int tov;
+    int plus_minus;
+
+    double underdog;
+    double defensive_effort;
+    double tp_affinity;
+    double longevity;
+
+
+
+    public Player(String first_name, String last_name, int gp, double mpg, double ppg,
+                  double rpg, double apg, double spg, double bpg, double tpg, double fgm,
+                  double fga, double fgp, double ftp, int min, int pts, int reb, int ast, int stl,
+                  int blk, int tov, int plus_minus){
+
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.gp = gp;
+        this.mpg = mpg;
+        this.ppg = ppg;
+        this.rpg = rpg;
+        this.apg = apg;
+        this.spg = spg;
+        this.bpg = bpg;
+        this.tpg = tpg;
+        this.fgm = fgm;
+        this.fga = fga;
+        this.fgp = fgp;
+        this.ftp = ftp;
+        this.min = min;
+        this.pts = pts;
+        this.reb = reb;
+        this.ast = ast;
+        this.stl = stl;
+        this.blk = blk;
+        this.tov = tov;
+        this.plus_minus = plus_minus;
+
+        underdog = (4*ppg + rpg + apg + 1.5*spg + 2.5*bpg)/mpg;
+
+        defensive_effort = (rpg + 1.5*bpg + 2*spg)/(fga);
+
+        tp_affinity = (2*tpa - fga)/mpg;
+
+        longevity = min + 100*(ppg-20) + (0.5)*(ast + blk + stl) - tov;
+
+    }
+
+
 }
